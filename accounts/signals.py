fbb46da0b78c688cd2130ee04947606f759f87e1
sasan-hsn <sasan.hasannia@gmail.com>
2026-08-07 15:28:57 +0330

    fix: auto-create TeacherProfile on teacher account creation #53

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,8 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from .models import StudentProfile
+from portfolio.models import TeacherProfile
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_student_profile(sender, instance, created, **kwargs):
-    if created and instance.role == instance.Role.STUDENT:
+def create_profile(sender, instance, created, **kwargs):
+    if not created:
+        return
+    if instance.role == instance.Role.STUDENT:
         StudentProfile.objects.create(user=instance)
+    elif instance.role == instance.Role.TEACHER:
+        TeacherProfile.objects.create(user=instance)
