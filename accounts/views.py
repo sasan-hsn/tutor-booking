8aada13f3ad5c0f46071da6c66a30e923c3ca523
sasan-hsn <sasan.hasannia@gmail.com>
2026-08-07 14:00:07 +0330

    feat: student signup view and form (#11)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from .forms import StudentSignUpForm
 
-# Create your views here.
+
+def student_signup(request):
+    if request.method == 'POST':
+        form = StudentSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('/')
+    else:
+        form = StudentSignUpForm()  
+    return render(request, 'accounts/student_signup.html', {'form': form})
