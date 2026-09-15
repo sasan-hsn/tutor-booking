2b5e8aa236acc6d3f39ec6373a9bbf76dc11a0c2
sasan-hsn <sasan.hasannia@gmail.com>
2026-09-15 20:17:34 +0330

    ops: add web healthcheck with autoheal and docker log rotation #141

--- tutor_booking/urls.py
+++ tutor_booking/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("portfolio.urls")),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('booking.urls')),
+    path('health/', health_check, name='health_check'),
 ]
 
 
