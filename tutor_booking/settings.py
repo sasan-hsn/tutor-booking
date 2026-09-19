a1ef00041ebb880591b7d3207f82d5b3d1175f1e
sasan-hsn <sasan.hasannia@gmail.com>
2026-09-19 18:24:24 +0330

    fix(auth): remove UserAttributeSimilarityValidator to relax password similarity restrictions #152

--- tutor_booking/settings.py
+++ tutor_booking/settings.py
@@ -88,9 +88,6 @@ if DATABASES['default']['ENGINE'] == 'django.db.backends.sqlite3':
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
